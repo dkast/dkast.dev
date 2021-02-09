ff9d78028cf24a3cd42796e254175880c0a9dd39
Daniel Castillejo <devcastillejo@gmail.com>
2021-02-09 00:04:41 -0600

    Remove unnecesary webpack custom config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,7 @@
-require("dotenv").config();
-const webpack = require("webpack");
-
 module.exports = {
   images: {
     domains: [
       "source.unsplash.com" //unsplash
     ]
-  },
-  webpack: config => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: "empty"
-    };
-
-    // .env plugin
-    config.plugins.push(new webpack.EnvironmentPlugin(process.env));
-
-    return config;
   }
 };
