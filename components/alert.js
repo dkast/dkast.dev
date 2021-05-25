b21241017c3dd0bf4a69859c51e3fc0e72641e47
Daniel Castillejo <devcastillejo@gmail.com>
2021-05-25 10:57:34 -0500

    Add style for dark mode

--- components/alert.js
+++ components/alert.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Alert = () => {
   return (
-    <div className="bg-yellow-100 text-yellow-500 py-1 w-full text-center">
+    <div className="bg-yellow-100 dark:bg-opacity-20 text-yellow-500 py-1 w-full text-center">
       {"Modo vista previa."}
       <a href="/api/exit-preview" className="ml-2 underline">
         Salir
